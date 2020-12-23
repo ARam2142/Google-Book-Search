c5b7fd048e00763a52f84cdb23acfdd147629628
Aram2142 <andresaaramirez@aol.com>
2020-12-23 00:43:55 -0500

    uncomment code

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ if (process.env.NODE_ENV === "production") {
 //Add routes, both API and view
 app.use(routes);
 
-// app.get("/googlebooks", function(req, res) {
-//   xios.get("https://www.googleapis.com/books/v1/volumes", { params: { q: 'knitting' } })
-//   .then(response => {
-//     res.json(response.data)
-//   })
-//   .catch(function (err) {
-//     console.log(err)
-//   });
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
-// });
 
 //connect to Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
