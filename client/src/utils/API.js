5fd68db3d8ce8886906aef0469a98e507b9a6325
Aram2142 <andresaaramirez@aol.com>
2020-12-23 17:52:20 -0500

    eliminate useless comments

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,37 +1,34 @@
+/* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
+//const API_Key = 'AIzaSyDICvg1AuEgsUWN9vSVLJ0MjImaJp7eT6E';
 
 export default {
-    // export function log(error) {
-    //     console.log(error);
-    // }
-    
-    //const API = {
-        // Gets all books
+
+        //Gets all books
         getBooks: function () {
             return axios.get("/api/books");
         },
+
         // Gets the book with the given id
         getBook: function (id) {
             return axios.get("/api/books/" + id);
         },
+
         // Deletes the book with the given id
         deleteBook: function (id) {
             return axios.delete("/api/books/" + id);
         },
     
         //searches a book
-        searchBook: function (booksSearched) {
-            console.log(booksSearched);
-            return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${booksSearched}:keyes&${API_Key}`)
+        searchBook: function (bookSearch) {
+            //console.log(bookSearch);
+            return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookSearch}`);
         },
         
         //Saves a book to the database
-          saveBook: function(bookSaved) {
-            return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookSaved}:keyes&${API_Key}`)
+          saveBook: function(bookData) {
+            return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookData}`)
           }
     
     };
-
-//}
-    //export default API;
 
