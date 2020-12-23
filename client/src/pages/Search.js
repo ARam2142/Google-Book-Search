5e398b35af6efd4cab5b35f8aa00c47a53020cb9
Aram2142 <andresaaramirez@aol.com>
2020-12-22 22:42:16 -0500

    book data returned in console

---
+++ client/src/pages/Search.js
@@ -0,0 +1,96 @@
+import react, { useEffect, useState } from "react";
+import API from "../utils/API";
+import Jumbotron from "../components/jumbotron/Jumbotron"
+import { Col, Row, Container } from "../components/grid/Grid";
+import { Input, FormBtn } from "../components/form/Index";
+import List from "../components/list/List";
+
+function Search() {
+    //setting the components initial state
+    const [bookSearch, setBookSearch] = useState(["sevilla"])
+    const [books, setBooks] = useState({});
+
+    //load all books and store them with setBooks
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    function loadBooks() {
+        API.searchBook()
+            .then(res =>
+                setBooks(res.data)
+            )
+            //console.log(res.data)
+            .catch(err => console.log(err));
+    };
+
+    // Handles updating component state when the user types into the input field
+    function handleInputChange(event) {
+        const { value } = event.target;
+        setBookSearch({ value })
+    };
+
+    //handles information being returned in the console
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (books) {
+            loadBooks();
+            console.log(books);
+        }
+    }
+
+    // function handleBookSubmit(books) {
+    //     if (bookSearch.link && bookSearch.image) {
+    //         API.saveBook({
+    //             title: books.title,
+    //             authors: books.authors,
+    //             description: books.description,
+    //             imageLinks: books.imageLinks,
+    //             link: books.link
+    //         }
+
+    //         )
+    //             .then(res => loadBooks())
+    //             .catch(err => console.log(err))
+    //     }
+
+    // }
+
+
+    //return info on page
+    //render() {
+    return (
+        <Container fluid>
+            <Row>
+                <Col size="sm-12">
+                    <Jumbotron>
+                        <h1>Welcome to Google Books</h1>
+                    </Jumbotron>
+                    <form>
+                        <Input
+                            onChange={handleInputChange}
+                            name="title"
+                            placeholder="Title (required)"
+                        />
+                        <FormBtn
+                            //disabled={!(formObject.author && formObject.title)}
+                            onClick={handleFormSubmit}
+                        >
+                            Submit
+                        </FormBtn>
+                    </form>
+                </Col>
+
+            </Row>
+            <List />
+
+        </Container>
+    )
+
+    //}
+
+
+
+}
+
+export default Search;
