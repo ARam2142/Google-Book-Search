a7c409d4647c74eb064581471da519344f829b63
Aram2142 <andresaaramirez@aol.com>
2020-12-27 20:29:46 -0500

    search page is displaying book information

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,4 +1,4 @@
-import react, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import API from "../utils/API";
 import Jumbotron from "../components/jumbotron/Jumbotron"
 import { Col, Row, Container } from "../components/grid/Grid";
@@ -8,89 +8,101 @@ import List from "../components/list/List";
 
 function Search() {
     //setting the components initial state
-    const [bookSearch, setBookSearch] = useState(["sevilla"])
-    const [books, setBooks] = useState({});
+    const [bookSearch, setBookSearch] = useState("sevilla");
+    const [books, setBooks] = useState([]);
 
     //load all books and store them with setBooks
     useEffect(() => {
         loadBooks()
-    }, [])
+    }, []);
 
     function loadBooks() {
         API.searchBook()
             .then(res =>
-                setBooks(res.data)
+                setBooks(res.data.items)
             )
-            //console.log(res.data)
             .catch(err => console.log(err));
     };
 
     // Handles updating component state when the user types into the input field
     function handleInputChange(event) {
         const { value } = event.target;
-        setBookSearch({ value })
+        setBookSearch({value})
     };
 
     //handles submitting component state when user submits form
     function handleFormSubmit(event) {
         event.preventDefault();
-        if (books) {
+        if (bookSearch) {
             loadBooks();
             console.log(books);
         }
     }
 
-    function handleBookSubmit(books) {
-        if (bookSearch.link && bookSearch.image) {
-            API.saveBook({
-                title: books.title,
-                authors: books.authors,
-                description: books.description,
-                image: books.imageLinks.thumbnail,
-                Link: books.infoLink
-            }
+    function handleBookSubmit(book) {
+        if (books.title) {
+            API.saveBook(
+                {
+                    title: book.title,
+                    authors: book.authors,
+                    description: book.description,
+                    image: book.imageLinks.thumbnail,
+                    link: book.infoLink
+                }
 
             )
-                .then(res => loadBooks())
+                .then(res => console.log(res))
                 .catch(err => console.log(err))
         }
 
     }
 
 
+    //modeled after Hillary Ferraro Google Books app
     //return info on page
-    //render() {
     return (
-        <Container fluid>
-            <Row>
-                <Col size="sm-12">
-                    <Jumbotron>
-                        <h1>Welcome to Google Books</h1>
-                    </Jumbotron>
-                    <form>
-                        <Input
-                            onChange={handleInputChange}
-                            name="title"
-                            placeholder="Title (required)"
-                        />
-                        <FormBtn
-                            //disabled={!(formObject.author && formObject.title)}
-                            onClick={handleFormSubmit}
-                        >
-                            Submit
+        <>
+            <Container fluid>
+                <Row>
+                    <Col size="sm-12">
+                        <Jumbotron>
+                            <h1>Welcome to Google Books</h1>
+                        </Jumbotron>
+                        <form>
+                            <Input
+                                onChange={handleInputChange}
+                                name="title"
+                                placeholder="Title (required)"
+                            />
+                            <FormBtn
+                                //disabled={!(formObject.author && formObject.title)}
+                                onClick={handleFormSubmit}
+                            >
+                                Search
                         </FormBtn>
-                    </form>
-                </Col>
-            </Row>
-            <List />
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+            <section>
+                <Container>
+                    <h2>Search Results</h2>
+                    <Row>
+                        {books ? books.map((book) => (
+                            <List
+                                key={book.id}
+                                info={book.volumeInfo}
+                                page={"search"}
+                                onClick={handleBookSubmit}
+                            />
+                        ))
+                        : null}
 
-        </Container>
-    )
-
-    //}
-
-
-
+                    </Row>
+                </Container>
+            </section>
+        </>
+    );
 }
 
 export default Search;
